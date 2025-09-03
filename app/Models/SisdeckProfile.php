39635696a4959c919d52c2b50da9c84185739040
decki-id <d.herdiawan.s@gmail.com>
2025-09-03 22:44:25 +0700

    Fix bug on Register feature

---
+++ app/Models/SisdeckProfile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SisdeckProfile extends Model
+{
+    protected $guarded = [];
+    
+    public function profileImage()
+    {
+        $dhsImagePath = ($this->image) ? $this->image : '/profile/default_user.png';
+        return '/storage/' . $dhsImagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable implements JWTSubject
             ]);
         });
     }
+
+    public function profile()
+    {
+        return $this->hasOne(SisdeckProfile::class);
+    }
 }
